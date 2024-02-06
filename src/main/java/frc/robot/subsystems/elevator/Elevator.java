8db7d5f04d975f5fcbbb4dc43699a35a8b292eb4
Fafnyer <maxcstone1@gmail.com>
2024-02-05 18:33:50 -0600

    started coding the Elevator subsystem

--- src/main/java/frc/robot/subsystems/elevator/Elevator.java
+++ src/main/java/frc/robot/subsystems/elevator/Elevator.java
@@ -1,3 +1,70 @@
 package frc.robot.subsystems.elevator;
 
-public class Elevator {}
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.lib.team6328.util.TunableNumber;
+
+public class Elevator extends SubsystemBase{
+
+    private CANSparkMax ElevatorLeftMotor;
+    private CANSparkMax ElevatorRightMotor;
+
+    private PIDController ElevatorLeftMotorPID;
+    private PIDController ElevatorRightMotorPID;
+
+    private ShuffleboardTab ElevatorTab;
+    
+    private TunableNumber ElevatorLeftMotorP;
+    private TunableNumber ElevatorLeftMotorI;
+    private TunableNumber ElevatorLeftMotorD;
+
+    private TunableNumber ElevatorRightMotorP;
+    private TunableNumber ElevatorRightMotorI;
+    private TunableNumber ElevatorRightMotorD;
+
+    public Elevator() {
+        ElevatorLeftMotor = new CANSparkMax(ElevatorConstants.ELEVATOR_LEFT_MOTOR_CAN_ID, MotorType.kBrushless);
+        ElevatorRightMotor = new CANSparkMax(ElevatorConstants.ELEVATOR_RIGHT_MOTOR_CAN_ID, MotorType.kBrushless);
+
+        ElevatorLeftMotorP = new TunableNumber("Elevator Left Motor P", ElevatorConstants.ELEVATOR_LEFT_MOTOR_P);
+        ElevatorLeftMotorI = new TunableNumber("Elevator Left Motor I", ElevatorConstants.ELEVATOR_LEFT_MOTOR_I);
+        ElevatorLeftMotorD = new TunableNumber("Elevator Left Motor D", ElevatorConstants.ELEVATOR_LEFT_MOTOR_D);
+
+        ElevatorRightMotorP = new TunableNumber("Elevator Right Motor P", ElevatorConstants.ELEVATOR_RIGHT_MOTOR_P);
+        ElevatorRightMotorI = new TunableNumber("Elevator Right Motor I", ElevatorConstants.ELEVATOR_RIGHT_MOTOR_I);
+        ElevatorRightMotorD = new TunableNumber("Elevator Right Motor D", ElevatorConstants.ELEVATOR_RIGHT_MOTOR_D);
+
+        ElevatorLeftMotorPID = new PIDController(ElevatorLeftMotorP.get(), ElevatorLeftMotorI.get(), ElevatorLeftMotorD.get());
+        ElevatorRightMotorPID = new PIDController(ElevatorRightMotorP.get(), ElevatorRightMotorI.get(), ElevatorRightMotorD.get());
+
+        
+        
+        if(ElevatorConstants.ELEVATOR_TESTING) {
+            setUpShuffleboard();
+        }
+    }
+
+
+
+    private void setUpShuffleboard() {
+        ElevatorTab = Shuffleboard.getTab("Elevator");
+
+        ElevatorTab.add("Elevator Left Motor P", ElevatorLeftMotorP);
+        ElevatorTab.add("Elevator Left Motor I", ElevatorLeftMotorI);
+        ElevatorTab.add("Elevator Left Motor D", ElevatorLeftMotorD);
+
+        ElevatorTab.add("Elevator Right Motor P", ElevatorRightMotorP);
+        ElevatorTab.add("Elevator Right Motor I", ElevatorRightMotorI);
+        ElevatorTab.add("Elevator Right Motor D", ElevatorRightMotorD);
+    }
+
+    public void periodic() {
+        // TODO Auto-generated method stub
+        super.periodic();
+    }
+}
--- src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
@@ -1,3 +1,20 @@
 package frc.robot.subsystems.elevator;
 
-public class ElevatorConstants {}
+public class ElevatorConstants {
+
+    public static final boolean ELEVATOR_TESTING = false;
+    
+    public static final int ELEVATOR_LEFT_MOTOR_CAN_ID = 1; //TODO: assign correct CAN IDs
+    public static final int ELEVATOR_RIGHT_MOTOR_CAN_ID = 2; //TODO: assign correct CAN IDs
+
+    public static final double ELEVATOR_LEFT_MOTOR_P = 0;
+    public static final double ELEVATOR_LEFT_MOTOR_I = 0;
+    public static final double ELEVATOR_LEFT_MOTOR_D = 0;
+
+    public static final double ELEVATOR_RIGHT_MOTOR_P = 0;
+    public static final double ELEVATOR_RIGHT_MOTOR_I = 0;
+    public static final double ELEVATOR_RIGHT_MOTOR_D = 0;
+
+    public static final double ELEVATOR_HIGH_SETPOINT = 0;
+    public static final double ELEVATOR_LOW_SETPOINT = 0;
+}
