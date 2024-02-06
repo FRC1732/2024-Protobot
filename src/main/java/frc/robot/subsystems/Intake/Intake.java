74c7a31d7a3727fa9285d0cc2baee6e84a6533d4
Yousef Abusamhadana <abusay6@muhs.edu>
2024-02-05 19:12:41 -0600

    intake subsystem & intake constants

---
+++ src/main/java/frc/robot/subsystems/Intake/Intake.java
@@ -0,0 +1,37 @@
+package frc.robot.subsystems.Intake;
+
+import com.revrobotics.CANSparkMax;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Intake extends SubsystemBase {
+  private CANSparkMax intakeLCanSparkMax;
+  private CANSparkMax intakeRCanSparkMax;
+
+  public static Double intakeMotorLSpeed = 0.40;
+  public static Double intakeMotorRSpeed = 0.40;
+
+  /** Creates a new Intake. */
+  public Intake() {
+
+    intakeLCanSparkMax = new CANSparkMax(IntakeConstants.INTAKE_MOTOR_L_CAN_ID,  CANSparkMax.MotorType.kBrushless);
+    intakeRCanSparkMax = new CANSparkMax(IntakeConstants.INTAKE_MOTOR_R_CAN_ID, CANSparkMax.MotorType.kBrushless);
+
+    intakeLCanSparkMax.setInverted(IntakeConstants.INTAKE_MOTOR_L_INVERTED);
+    intakeRCanSparkMax.setInverted(IntakeConstants.INTAKE_MOTOR_R_INVERTED);    
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  public void runIntake() {
+    intakeLCanSparkMax.set(intakeMotorLSpeed);
+    intakeRCanSparkMax.set(intakeMotorRSpeed);
+  }
+
+  public void stopIntake() {
+    intakeLCanSparkMax.set(0);
+    intakeRCanSparkMax.set(0);
+  }
+}
