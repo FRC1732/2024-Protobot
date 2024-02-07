d869a52214d2ed82c5a13512c20c61a63e3e3963
Fafnyer <maxcstone1@gmail.com>
2024-02-06 19:03:20 -0600

    Finished splitting shooter wheels and pose

--- src/main/java/frc/robot/subsystems/shooterWheels/ShooterWheelsConstants.java
+++ src/main/java/frc/robot/subsystems/shooterWheels/ShooterWheelsConstants.java
@@ -1,10 +1,10 @@
-package frc.robot.subsystems.shooter;
+package frc.robot.subsystems.shooterWheels;
 
-public class ShooterConstants {
+public class ShooterWheelsConstants {
 
   private static final String CONSTRUCTOR_EXCEPTION = "constant class";
 
-  private ShooterConstants() {
+  private ShooterWheelsConstants() {
     throw new IllegalStateException(CONSTRUCTOR_EXCEPTION);
   }
 
@@ -20,14 +20,4 @@ public class ShooterConstants {
   public static final double SHOOTER_SPEED_115 = 0.0;
   public static final double SHOOTER_SPEED_125 = 0.0;
   public static final double SHOOTER_SPEED_150 = 0.0;
-
-  public static final double SHOOTER_TILT_P = 0;
-  public static final double SHOOTER_TILT_I = 0;
-  public static final double SHOOTER_TILT_D = 0;
-
-  public static final double SHOOTER_TILT_SPEAKER_115_SETPOINT = 0;
-  public static final double SHOOTER_TILT_SPEAKER_125_SETPOINT = 0;
-  public static final double SHOOTER_TILT_SPEAKER_150_SETPOINT = 0;
-  public static final double SHOOTER_TILT_AMP_SETPOINT = 0;
-  public static final double SHOOTER_TILT_TRAP_SETPOINT = 0;
 }
