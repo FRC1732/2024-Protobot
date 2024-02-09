60ad248bf029910851ceecb2d7cf205cfe44f393
Theodore Kranz <kranzt17@muhs.edu>
2024-02-08 19:47:42 -0600

    use hasNote

--- src/main/java/frc/robot/commands/FeedShooter.java
+++ src/main/java/frc/robot/commands/FeedShooter.java
@@ -10,8 +10,6 @@ import frc.robot.subsystems.feeder.Feeder;
 
 public class FeedShooter extends Command {
   private final Feeder feederSystem;
-  private final AnalogInput analog;
-
 
   /** Creates a new FeedShooter. 
     * @param feeder the drivetrain subsystem required by this command
@@ -20,7 +18,6 @@ public class FeedShooter extends Command {
   public FeedShooter(Feeder feeder) {
     addRequirements(feeder);
     feederSystem = feeder;
-    analog = new AnalogInput(0);
   }
 
   // Called when the command is initially scheduled.
@@ -40,7 +37,7 @@ public class FeedShooter extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    if (analog.getValue() > 700) {
+    if (feederSystem.hasNote()) {
       feederSystem.stopFeederIn();
       return true;
     } else {
