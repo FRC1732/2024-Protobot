083602923fdc86fb80d4a2b4f56b08f6ad73cd59
Theodore Kranz <kranzt17@muhs.edu>
2024-02-10 14:26:01 -0600

    make stopshooter not use a deprecated method and make it end after it starts as in commands.md

--- src/main/java/frc/robot/commands/shooterCommands/StopShooter.java
+++ src/main/java/frc/robot/commands/shooterCommands/StopShooter.java
@@ -5,6 +5,8 @@
 package frc.robot.commands.shooterCommands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.shooterPose.Pose;
+import frc.robot.subsystems.shooterWheels.ShooterSpeed;
 import frc.robot.subsystems.shooterWheels.ShooterWheels;
 
 public class StopShooter extends Command {
@@ -20,7 +22,7 @@ public class StopShooter extends Command {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    shooterWheels.rampDownShooter();
+    shooterWheels.setShooterSpeed(ShooterSpeed.STOPPED);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
@@ -34,6 +36,6 @@ public class StopShooter extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return true;
   }
 }
