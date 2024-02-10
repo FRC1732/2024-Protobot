4d96e99bbf939d02c45a51f198e334ef76c57816
Leo Cutraroe <leocutraro@gmail.com>
2024-02-10 10:25:23 -0600

    Setup basic file for SetShooterDistance

---
+++ src/main/java/frc/robot/commands/SetShooterDistance.java
@@ -0,0 +1,25 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.shooterPose.ShooterPose;
+
+public class SetShooterDistance extends Command {
+private final ShooterPose shooterPose;
+private double distanceInches;
+
+    public SetShooterDistance(ShooterPose shooterPose, double distanceInches) {
+        this.shooterPose = shooterPose;
+    }
+    public void initialize() {
+
+    }
+    public void execute() {
+    
+    }
+    public void end(boolean isInterupted) {
+
+    }
+    public boolean isFinnished() {
+        return true;
+    }
+}
