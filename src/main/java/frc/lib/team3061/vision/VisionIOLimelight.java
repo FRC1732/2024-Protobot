2cc7f344e6449b1a184dd5db41f0c5fee41bca75
Alan Bork <aebork@gmail.com>
2024-02-18 13:54:38 -0600

    Moved Limelight's VisionIO with the rest of VisionIO.

--- src/main/java/frc/lib/team3061/vision/VisionIOLimelight.java
+++ src/main/java/frc/lib/team3061/vision/VisionIOLimelight.java
@@ -2,12 +2,12 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.limelightVision;
+package frc.lib.team3061.vision;
 
 import edu.wpi.first.wpilibj.Timer;
-import frc.lib.team3061.vision.VisionIO;
 import frc.lib.team6328.util.Alert;
 import frc.lib.team6328.util.Alert.AlertType;
+import frc.robot.limelightVision.LimelightHelpers;
 import frc.robot.limelightVision.LimelightHelpers.LimelightResults;
 
 public class VisionIOLimelight implements VisionIO {
